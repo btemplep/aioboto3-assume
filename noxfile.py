d798df7ebbb2e2cc41d265499548b0886fdef453
btemplep <btemplepgit@gmail.com>
2026-01-14 01:11:46 -0500

    nox file fixed. Initial release

--- noxfile.py
+++ noxfile.py
@@ -1,13 +1,11 @@
 
-import sys
-
 import nox
 
 nox.options.sessions = [
     "unit-tests-py"
 ]
 
-@nox.session(name="publish-package")
+@nox.session(name="publish")
 def publish_package(session: nox.Session):
     """Build a new src dist and wheel, then publish to PYPI.
     """
